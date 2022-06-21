dada52a1ad8b91075c411d48efbd7793136140ef
Junsuk Park <park64kr63@gmail.com>
2022-06-21 21:54:56 +0900

    git init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import UserInfoForm from "./containers/UserInfoForm";
+
+function App() {
+  return (
+    <div className="App">
+      <UserInfoForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/types/InputProps.ts
@@ -0,0 +1,9 @@
+import {HTMLInputTypeAttribute} from "react";
+
+export interface InputProps {
+    source: string;
+    label: string;
+    placeholder?: string;
+    type?: HTMLInputTypeAttribute
+    // validate?: any[];
+}
